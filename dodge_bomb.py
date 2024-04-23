1352155a76e52e3f4c0d9d7a88ed526960d91962
C0B23008 <c0b23008ff@edu.teu.ac.jp>
2024-04-23 17:27:26 +0900

    問題4　途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,18 +1,17 @@
 import os
-import sys
 import random
+import sys
 import time
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-DELTA = { # 移動量辞書（押下キー　：　移動量タプル）
+DELTA = { # 移動量辞書（押下キー：移動量タプル）
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
-
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -28,7 +27,6 @@ def check_bound(obj_rct) -> tuple[bool, bool]:
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
-
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -49,18 +47,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        if kk_rct.colliderect(bd_rct):  # こうかとんと爆弾がぶつかったら
-            GameOver()
+        if kk_rct.colliderect(bd_rct):  #  こうかとんと爆弾がぶつかったら
+            game_over() #  ゲームオーバーの関数を呼び出す
             return
         screen.blit(bg_img, [0, 0]) 
-
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
-
         #if key_lst[pg.K_UP]:
         #    sum_mv[1] -= 5
         #if key_lst[pg.K_DOWN]:
@@ -84,32 +80,31 @@ def main():
         tmr += 1
         clock.tick(50)
 
-def GameOver():
+def game_over():
     """
     ゲームオーバーになったときに発動する関数
     暗く半透明のバックグラウンド+文字+こうかとん
     """
     fonto = pg.font.Font(None, 80)
     txt = fonto.render("Game Over", True, (255, 255, 255))
-    clock = pg.time.Clock()
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg") 
+    bg_img.set_alpha(100)
     end_kk_rct = pg.image.load("fig/8.png")
     end_kk_rct = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0) 
-    bg_img.set_alpha(100)
-    screen.blit(bg_img, [0, 0])
-    screen.blit(txt, [WIDTH/2, HEIGHT/2])
-    screen.blit(end_kk_rct, [WIDTH/2-100, HEIGHT/2])
-    screen.blit(end_kk_rct, [WIDTH/2+350, HEIGHT/2])
+    screen.blit(bg_img, [0, 0]) #  バックグラウンドを表示
+    screen.blit(txt, [WIDTH/2, HEIGHT/2]) #  "Game Over"を表示
+    screen.blit(end_kk_rct, [WIDTH/2-100, HEIGHT/2]) #  こうかとんを表示
+    screen.blit(end_kk_rct, [WIDTH/2+350, HEIGHT/2]) #  こうかとんを表示
     pg.display.update()
     print("Gmame Over")
     time.sleep(5)
     #clock.tick(1/5)
 
-def kasoku():
+def kasoku_kyodai():
     """
-    爆弾の速度を加速させる関数
-    10段階に分けて速度を加速する
+    爆弾の速度を加速させる関数、また爆弾を拡大する関数
+    10段階に分けて速度を加速する、また爆弾を拡大する
     """
     acc = [a for a in range(1, 11)]
     for r in range(1, 11):
